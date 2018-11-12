5a4ed2d438eee89b65f9ff06b820bd5bf43dff86
Stoyan <s.chepov@abv.bg>
2018-11-12 18:31:32 +0200

    baseurl wants to work!

--- gulpfile.js
+++ gulpfile.js
@@ -4,71 +4,47 @@ var browserSync = require('browser-sync').create();
 var $ = require('gulp-load-plugins')();
 
 var path = {
-	SCSS_SRC	: './scss/**/*.scss',
-	SCSS_DST	: './css',
-<<<<<<< HEAD
-	HTML_SRC	: ['./css/*.css','./*.html','./_post/*.*','./_layouts/*.*', './_includes/*.*'],
-}
-
-gulp.task('sass', function () {
-	
-	gulp.src( path.SCSS_SRC )
-	.pipe($.sass())
-	.pipe($.autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
-	.pipe($.size({ showFiles: true }))
-	.pipe($.csso())
-	.pipe($.size({ showFiles: true }))
-	.pipe($.sourcemaps.write('map'))
-	.pipe(gulp.dest( path.SCSS_DST ))
-	.pipe(browserSync.stream({ match: '**/*.css' }))
-=======
-	HTML_SRC	: ['./css/*.css','./*.html','./_posts/*.*','./_layouts/*.*', './_includes/*.*'],
+	SCSS_SRC: './scss/**/*.scss',
+	SCSS_DST: './css',
+	CSS_JKDST: './docs/css',
+	HTML_SRC: ['./*.html', './_post/*.*', './_layouts/*.*', './_includes/*.*'],
 }
 
 gulp.task('scss', function () {
-	
-	gulp.src( path.SCSS_SRC )
-	    .pipe($.sass())
-	    .pipe($.autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
-	    .pipe($.size({ showFiles: true }))
-	    .pipe($.csso())
-	    .pipe($.size({ showFiles: true }))
-	    .pipe($.sourcemaps.write('map'))
-	    .pipe(gulp.dest( path.SCSS_DST ))
-	    .pipe(browserSync.stream({ match: '**/*.css' }))
->>>>>>> 8d31ab6259eaaa7327886c745662032aa70a1f72
-	;
-	
+
+	gulp.src(path.SCSS_SRC)
+		.pipe($.sourcemaps.init())
+		.pipe($.sass())
+		.pipe($.autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
+		.pipe($.size({ showFiles: true }))
+		.pipe($.csso())
+		.pipe($.size({ showFiles: true }))
+		.pipe($.sourcemaps.write('map'))
+		.pipe(gulp.dest(path.SCSS_DST))
+		.pipe(gulp.dest(path.CSS_JKDST))
+		.pipe(browserSync.stream({ match: '**/*.css' }))
+		;
+
 });
 
 gulp.task('jekyll', function () {
-<<<<<<< HEAD
-	require('child_process').exec('jekyll build --baseurl=', {stdio: 'inherit'});
-=======
-	require('child_process').exec('bundle exec jekyll build --baseurl=', {stdio: 'inherit'});
->>>>>>> 8d31ab6259eaaa7327886c745662032aa70a1f72
+	require('child_process').exec('jekyll build --baseurl=', { stdio: 'inherit' }, function () {
+		browserSync.reload();
+	});
 });
 
-gulp.task('serve', function() {
-	
+gulp.task('serve', function () {
+
 	browserSync.init({
 		server: {
 			baseDir: "./docs/"
 		}
 	});
-	
-<<<<<<< HEAD
-	gulp.watch(path.SCSS_SRC, ['sass']);
-=======
-	gulp.watch(path.SCSS_SRC, ['scss', 'jekyll']);
->>>>>>> 8d31ab6259eaaa7327886c745662032aa70a1f72
+
+	gulp.watch(path.SCSS_SRC, ['scss']);
 	gulp.watch(path.HTML_SRC, ['jekyll']);
 	gulp.watch(path.HTML_SRC).on('change', browserSync.reload);
-	
+
 });
 
-<<<<<<< HEAD
-gulp.task('default', ['jekyll','serve']);
-=======
-gulp.task('default', ['scss','jekyll','serve']);
->>>>>>> 8d31ab6259eaaa7327886c745662032aa70a1f72
+gulp.task('default', ['scss', 'jekyll', 'serve']);
